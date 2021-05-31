04483eeca0b51ca6964e8bd83b4d592c6e9de171
victor <gictorbit@gmail.com>
2021-05-31 12:11:55 +0430

    enable internet sharing function using iptables added

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/Packetify/ipcalc v0.0.0-20210426125024-8df832c7d139
+	github.com/coreos/go-iptables v0.6.0
 	github.com/krolaw/dhcp4 v0.0.0-20190909130307-a50d88189771
 	github.com/mitchellh/mapstructure v1.1.2
 	github.com/spf13/viper v1.7.1
--- networkHandler/netHandle.go
+++ networkHandler/netHandle.go
@@ -2,6 +2,7 @@ package networkHandler
 
 import (
 	"fmt"
+	"github.com/coreos/go-iptables/iptables"
 	"net"
 	"os/exec"
 	"strings"
@@ -24,4 +25,32 @@ func GetAdapterKernelModule(iface net.Interface) string {
 	modulePath, _ := exec.Command("readlink", "-f", fmt.Sprintf("/sys/class/net/%s/device/driver/module", iface.Name)).Output()
 	modName := strings.Split(string(modulePath), "/")
 	return modName[len(modName)-1]
+}
+
+func EnableInternetSharing(iface string, netSahreIface string, ipRange net.IPNet, flush bool) error {
+	ipt, _ := iptables.New()
+	if flush {
+		if err := ipt.ClearAll(); err != nil {
+			return err
+		}
+	}
+	if err := ipt.Insert("nat", "POSTROUTING", 1, "-w", "-s", ipRange.String(), "!", "-o", iface, "-j", "MASQUERADE"); err != nil {
+		return err
+	}
+
+	if err := ipt.Insert("filter", "FORWARD", 1, "-i", iface, "-s", ipRange.String(), "-j", "ACCEPT"); err != nil {
+		return err
+	}
+	if err := ipt.Insert("filter", "FORWARD", 1, "-i", netSahreIface, "-d", ipRange.String(), "-j", "ACCEPT"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func IPTablesFlash()error{
+	ipt, _ := iptables.New()
+	if err := ipt.ClearAll(); err != nil {
+		return err
+	}
+	return nil
 }
